d6379d622e99f6abecad1a1b51e89f42f2a48350
igor-stefan <igorstefan@aol.com>
2023-11-15 08:37:45 -0300

    adiciona bibliotecas utilizadas

--- modelagemZika.h
+++ modelagemZika.h
@@ -1,9 +1,18 @@
+#include <sys/stat.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <random>
+    
+
 #ifndef ZIKA_H
 #define ZIKA_H
-    
+
     double calcularDeslocamentoHorizontal(double pressaoHemodinamica, double velocidadeInicial, double incrementoTempo, double coeficienteDifusao, double incrementoWiener);
     double calcularDeslocamentoVertical(int forca, double pressaoVenosa, double incrementoTempo, double coeficienteDifusao, double incrementoWiener);
     bool atendeCriterioParadaY(const double resistenciaVascular[], const int rota[], int arteriaAtual, double posYAtual);
     bool atendeCriterioParadaX(const double totalD[], const double comprimentoArteria[], const int rota[], int arteriaAtual, double posXAtual);
     bool atendeCriterioAbsorcaoReflexao(const double resistenciaVascular[], const int rota[], const int arteriaAtual, double posAtualY);
+
 #endif
